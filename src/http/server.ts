3d7999349cc91256fa07cff1d11ed8b7c56a2243
Omar Colombari <omarucolombari@gmail.com>
2024-12-26 13:39:39 -0300

    feat: make create completion route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,17 @@
 import fastify from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { createGoalRoute } from './routes/create-goal'
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(createGoalRoute)
 
 app
   .listen({
