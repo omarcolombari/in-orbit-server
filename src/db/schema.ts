d06518a4b961e3eba8c6f43bdbb24be2b2923767
Omar Colombari <omarucolombari@gmail.com>
2024-12-30 19:23:42 -0300

    feat: create schema and migration for users table

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,6 +1,16 @@
 import { pgTable, text, integer, timestamp } from 'drizzle-orm/pg-core'
 import { createId } from '@paralleldrive/cuid2'
 
+export const users = pgTable('users', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => createId()),
+  name: text('name'),
+  email: text('email'),
+  avatarUrl: text('avatar_url').notNull(),
+  externalAccountId: integer('external_account_id').notNull().unique(),
+})
+
 export const goals = pgTable('goals', {
   id: text('id')
     .primaryKey()
